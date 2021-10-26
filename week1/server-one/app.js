c9a4c3a3150f7040a21db7b46742cdc9b1e37d88
dieu-vu <vuphuongdieu212@gmail.com>
2021-10-26 22:03:55 +0300

    use strict ; in app.js

--- week1/server-one/app.js
+++ week1/server-one/app.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+'use strict';
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
 app.get('/catinfo', (req, res) => {
   const cat = {
     name: 'Frank',
