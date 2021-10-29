00bf8450173f679296638d93962e189efcd666c0
dieu-vu <vuphuongdieu212@gmail.com>
2021-10-29 14:30:31 +0300

    make better pug

--- week1/server-two/app.js
+++ week1/server-two/app.js
@@ -3,23 +3,26 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'pug');
 app.use(express.static('public'));
 const pug = require('pug');
-app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
-  res.render('index', {name: 'con meo', age: 100, weight: 50})
-})
+  res.render('index',{title: 'Title', pageheading: 'Click on the cat'});
+});
 
+app.get('/page2', (req,res) => {
+	res.render('page2', {title:'Title', pageheading: 'Hello from page 2'});
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
-/*app.get('/catinfo', (req, res) => {
+app.get('/catinfo', (req, res) => {
   const cat = {
     name: 'Frank',
     birthdate: '2010-12-25',
     weight: 5,
   };
   res.json(cat);
-});*/
+});
