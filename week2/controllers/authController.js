5d8c0f881a2b415f247b7cb1ebb38f83bf372c2f
dieu-vu <vuphuongdieu212@gmail.com>
2021-11-19 08:51:45 +0200

    correct authController error handling

--- week2/controllers/authController.js
+++ week2/controllers/authController.js
@@ -2,25 +2,26 @@
 
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
+const {httpError} = require('../utils/errors');
 
 const login = (req, res, next) => {
   // TODO: add passport authenticate
 	passport.authenticate('local', {session: false}, 
 		(err, user, info) => {
 			if( err || !user){
-				next(err);		
+				next(httpError('username/password incorrect',400));		
 				return;
 			}	
 			
 			req.login(user, {session: false}, (err) => {
-				next(err);
+				next(httpError('login error',400));
 				return;
 			});
 			
 			const token = jwt.sign(user,'foobar');
 			return res.json({user, token});
 		}
-	)(req, res);
+	)(req, res, next);
 };
 
 module.exports = {
